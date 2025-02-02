8bb0731fe05db1a0fe2add870525bb20e0a91148
achensen <Erinrjacobsen@gmail.com>
2025-02-02 16:28:37 -0500

    no more can

--- src/pages/SavedCandidates.tsx
+++ src/pages/SavedCandidates.tsx
@@ -1,40 +1,47 @@
 import { useEffect, useState } from "react";
 import type { Candidate } from "../interfaces/Candidate.interface";
-import './savedCandidates.css'
+import "./savedCandidates.css";
 
 const SavedCandidates = () => {
-  const [savedUsers, setSavedUsers] = useState<Candidate[]>([])
+  const [savedUsers, setSavedUsers] = useState<Candidate[]>([]);
   const readLocalStorage = () => {
-    const localStorageUsers: any= localStorage.getItem('users') 
-    if (localStorageUsers){
-      return JSON.parse(localStorageUsers)
+    const localStorageUsers: any = localStorage.getItem("users");
+    if (localStorageUsers) {
+      return JSON.parse(localStorageUsers);
     } else {
-      return []
+      return [];
     }
-  }
-  const removeUser= (userLogin: string)=> {
-    const localStorageUsers = readLocalStorage()
-    const filteredUsers = localStorageUsers.filter((user:Candidate)=>user.login !== userLogin)
-    localStorage.setItem('users', JSON.stringify(filteredUsers))
-    setSavedUsers(filteredUsers)
-  }
+  };
+  const removeUser = (userLogin: string) => {
+    const localStorageUsers = readLocalStorage();
+    const filteredUsers = localStorageUsers.filter(
+      (user: Candidate) => user.login !== userLogin
+    );
+    localStorage.setItem("users", JSON.stringify(filteredUsers));
+    setSavedUsers(filteredUsers);
+  };
   useEffect(() => {
-    const localStorageUsers = readLocalStorage()
-    setSavedUsers(localStorageUsers)
-  }, [])
-  
+    const localStorageUsers = readLocalStorage();
+    setSavedUsers(localStorageUsers);
+  }, []);
+
   return (
     <>
       <h1>Potential Candidates</h1>
       <div>
-        {savedUsers.map((user)=>{
-          return <div key={user.login} className="savedUserCard">
-            <div>{user.login}</div>
-            <img src={user.avatar_url} alt="" />
-            <button onClick={()=>removeUser(user.login)}>-</button>
-            </div>
-
-        })}
+        {savedUsers.length > 0 ? (
+          savedUsers.map((user) => {
+            return (
+              <div key={user.login} className="savedUserCard">
+                <div>{user.login}</div>
+                <img src={user.avatar_url} alt="" />
+                <button onClick={() => removeUser(user.login)}>-</button>
+              </div>
+            );
+          })
+        ) : (
+          <div>No Potential Candidates</div>
+        )}
       </div>
     </>
   );
